c3b870d99205da47a5de8712b1d53859c6a621b0
Leo Cheung <lappangcheung@gmail.com>
2024-01-14 17:34:58 -0500

    Setting the shopping card and skeleton out the cart item component

---
+++ components/CartItem.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { useUserStore } from '~/store/user.js'
+
+const props = defineProps(['product', 'selectedArray'])
+const { product, selectedArray } = toRefs(props)
+
+const emit = defineEmits(['selectedRadio'])
+const userStore = useUserStore()
+
+let isHover = ref(false)
+let isSelected = ref(false)
+
+const removeFromCart = () => {
+  userStore.cart.forEach((prod, index) => {
+    if (prod.id === product.value.id) {
+      userStore.cart.splice(index, 1)
+    }
+  })
+}
+
+watch(() => isSelected.value, (val) => {
+  emit('selectedRadio', { id: product.value.id, isSelected: val })
+})
+</script>
+
+<template>
+  <div class="flex justify-start my-2">
+    <div class="my-auto">
+      <div @mouseenter="isHover = true"
+           @mouseleave="isHover = false"
+           class="flex items-center justify-start p-0.5 cursor-pointer">
+        <div @click="isSelected = !isSelected"
+             class="flex items-center justify-center h-[20px] w-[20px] rounded-full border mr-5 ml-2"
+             :class="[
+                 isHover ? 'border-[#fd374f]' : 'border-gray-300',
+                 isSelected ? 'bg-[#fd374f]' : ''
+             ]">
+          <div class="h-[8px] w-[8px] rounded-full bg-slate-100" />
+        </div>
+      </div>
+    </div>
+    <img class="rounded-md md:w-[150px] w-[90px]"
+         alt="product item"
+         :src="product.url"/>
+    <div class="overflow-hidden pl-2 w-full">
+      <div class="flex items-center justify-between w-full">
+        <div class="flex items-center justify-between truncate">
+          <span class="sm:block hidden bg-[#fd374f] text-white text-[9px] font-semibold px-1.5 rounded-sm min-w-[80px]">
+            Welcome Deal
+          </span>
+          <div class="truncate sm:pl-2">
+            {{ product.title }}
+          </div>
+        </div>
+        <button @click="removeFromCart()"
+                class="mx-3 sm:block hidden -mt-0.5 hover:text-red-500">
+          <Icon name="material-symbols:delete-outline"
+                size="20" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
