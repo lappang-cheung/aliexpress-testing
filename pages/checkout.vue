1a5317a1622331ad291716954026ed86afc75147
Leo Cheung <lappangcheung@gmail.com>
2024-01-14 20:12:30 -0500

    Checkout page setting up

---
+++ pages/checkout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
+
+<template>
+  <MainLayout>
+    <div>
+      Checkout
+    </div>
+  </MainLayout>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
