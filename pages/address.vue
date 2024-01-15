5679eda89e82545020375af5d438feb91128fdb9
Leo Cheung <lappangcheung@gmail.com>
2024-01-14 21:15:55 -0500

    Setting up the address

---
+++ pages/address.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MainLayout from '~/layouts/MainLayout'
+</script>
+
+<template>
+  <MainLayout>
+    <div>
+      Address
+    </div>
+  </MainLayout>
+</template>
+
+<style lang="scss" scoped>
+</style>
