84a6e92a696c35c5fce4ce9c76cd118a5be26d38
Leo Cheung <lappangcheung@gmail.com>
2024-01-14 21:12:44 -0500

    Finished out the check out

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -131,6 +131,53 @@ onMounted(() => {
             </div>
           </div>
         </div>
+        <div class="md:hidden block my-4" />
+        <div class="md:w-[35%]">
+          <div id="PlaceOrder" class="bg-white rounded-lg p-4 border">
+            <div class="text -2xl font-extrabold mb-2">
+              Summary
+            </div>
+            <div class="flex items-center justify-between my-4">
+              <div>Total Shipping</div>
+              <div>Free</div>
+            </div>
+            <div class="border-t" />
+            <div class="flex items-center justify-between my-4">
+              <div class="font-semi-bold">
+                Total
+              </div>
+              <div class="text-2xl font-semibold">
+                $ <span class="font-extrabold">
+                  {{ total / 100 }}
+                </span>
+              </div>
+            </div>
+            <form @submit.prevent="pay()">
+              <div class="border border-gray-500 p-2 rounded-sm"
+                   id="card-element" />
+              <p class="text-red-700 text-center font-semibold"
+                 role="alert"
+                 id="card-error" />
+              <button :disabled="isProcessing"
+                      type="submit"
+                      class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                      :class="isProcessing ? 'opacity-70' : 'opacity-100'">
+                <Icon v-if="isProcessing" name="eos-icons:loading" size="20" class="mr-2" />
+                <div v-else>
+                  Place order
+                </div>
+              </button>
+            </form>
+          </div>
+          <div class="bg-white rounded-lg p-4 mt-4 border">
+            <div class="text-lg font-semibold mb-2 mt-2">
+              AliExpress
+            </div>
+            <p class="my-2">
+              AliExpress keeps your information and payment safe
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </MainLayout>
