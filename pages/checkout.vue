b98c4be4c3e3435bb607415228555b9a79e2f848
Leo Cheung <lappangcheung@gmail.com>
2024-01-23 23:08:17 -0500

    Setting up the stripe for checkout

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -15,21 +15,92 @@ let clientSecret = null
 let currentAddress = ref(null)
 let isProcessing = ref(false)
 
-const stripeInit = async() => {
+const stripeInit = async () => {
+  const runtimeConfig = useRuntimeConfig()
+  stripe = Stripe(runtimeConfig.stripePk);
 
+  let res = await $fetch('/api/stripe/paymentintent', {
+    method: 'POST',
+    body: {
+      amount: total.value,
+    }
+  })
+  clientSecret = res.client_secret
+
+  elements = stripe.elements();
+  let style = {
+    base: {
+      fontSize: "18px",
+    },
+    invalid: {
+      fontFamily: 'Arial, sans-serif',
+      color: "#EE4B2B",
+      iconColor: "#EE4B2B"
+    }
+  };
+  card = elements.create("card", {
+    hidePostalCode: true,
+    style: style
+  });
+
+  // Stripe injects an iframe into the DOM
+  card.mount("#card-element");
+  card.on("change", function (event) {
+    // Disable the Pay button if there are no card details in the Element
+    document.querySelector("button").disabled = event.empty;
+    document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+  });
+
+  isProcessing.value = false
 }
 
-const pay = async() => {
+const pay = async () => {
+  if (currentAddress.value && currentAddress.value.data === '') {
+    showError('Please add shipping address')
+    return
+  }
+  isProcessing.value = true
 
+  let result = await stripe.confirmCardPayment(clientSecret, {
+    payment_method: { card: card },
+  })
+
+  if (result.error) {
+    showError(result.error.message);
+    isProcessing.value = false
+  } else {
+    await createOrder(result.paymentIntent.id)
+    userStore.cart = []
+    userStore.checkout = []
+    setTimeout(() => {
+      return navigateTo('/success')
+    }, 500)
+  }
 }
 
-const createOrder = async(stripeId) => {
-
+const createOrder = async (stripeId) => {
+  await useFetch('/api/prisma/create-order', {
+    method: "POST",
+    body: {
+      userId: user.value.id,
+      stripeId: stripeId,
+      name: currentAddress.value.data.name,
+      address: currentAddress.value.data.address,
+      zipcode: currentAddress.value.data.zipcode,
+      city: currentAddress.value.data.city,
+      country: currentAddress.value.data.country,
+      products: userStore.checkout
+    }
+  })
 }
 
 const showError = (errorMsgText) => {
+  let errorMsg = document.querySelector("#card-error");
 
-}
+  errorMsg.textContent = errorMsgText;
+  setTimeout(() => { errorMsg.textContent = "" }, 4000);
+};
+
 
 watch(() => total.value, () => {
   if (total.value > 0) {
